59a9cdc6b32cfc64878c4a6c2f25b706d9551647
Gebin Maxey <gebinmaxey@gmail.com>
2018-04-08 20:50:56 -0600

    remove draw and add show

--- ram.py
+++ ram.py
@@ -16,4 +16,4 @@ ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
 plt.title('Ram Usage')
-plt.draw()
+plt.show()
